8fda925f6bc480fa115bfe1e2a6bc5d0aeed3de8
Kim Sangyeob <yeob32@gmail.com>
2021-07-20 20:30:51 +0900

    remove unuse file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { HelloModule } from './hello/hello.module';
 
 @Module({
   imports: [HelloModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
